a4b1c1f48bcdde9df8a06b36acea6896976867e4
Meharunnesa <meharunnesab28@gmail.com>
2025-05-06 11:17:17 +0600

    insert date and time into database using oop

---
+++ easycommerce-delivery-date-picker.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Plugin Name:       Delivery Date Picker
+ * Description:       This plugin help for pick delivery date
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Bristy
+ * Author URI:        https://author.example.com/
+ * Text Domain:       delivery-date-picker
+ * Domain Path:       /languages
+ */
+
+ require_once __DIR__ . '/inc/picker.php';
+
+class Delivery_Date_Picker {
+
+    public function __construct() {
+        add_action( 'init' , [ $this , 'picker' ] );
+        //add_action( 'easycommerce_after_cart_items' , [ $this , 'picker' ] );
+    }
+
+    public function picker() {
+        $picker = new Picker;
+        $picker->date_time_picker();
+    }
+}
+
+new Delivery_Date_Picker();
+
+?>
+
+
---
+++ inc/picker.php
@@ -0,0 +1,45 @@
+<?php
+
+class Picker {
+
+    public function date_time_picker() {
+        ?>
+
+        <form action="" method="post">
+            <h2>Delivery Date Picker</h2>
+            <label for="">Date : </label>
+            <input type="date" name="date" id="">
+            <label for="">Time : </label>
+            <input type="time" name="time" id="">
+            <input type="submit" value="Submit" name="submit_delivery">
+        </form>
+    
+        <?php
+    
+        if (isset($_POST['submit_delivery'])) {
+    
+            global $wpdb;
+            $table_name = $wpdb->prefix . 'date_time';
+    
+            $data = array(
+                'date' => sanitize_text_field($_POST['date']),
+                'time' => sanitize_text_field($_POST['time'])
+            );
+            $format = array('%s', '%s');
+    
+            $wpdb->insert($table_name, $data, $format);
+    
+        } 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+?>
