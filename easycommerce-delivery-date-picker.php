cd69f56c663075fbc2e31f0c528b0c5369773066
Meharunnesa <meharunnesab28@gmail.com>
2025-05-07 12:13:39 +0600

    save date and time into database

--- easycommerce-delivery-date-picker.php
+++ easycommerce-delivery-date-picker.php
@@ -11,21 +11,37 @@
  * Domain Path:       /languages
  */
 
- require_once __DIR__ . '/inc/picker.php';
+require_once __DIR__ . '/inc/picker.php';
+use EasyCommerce\API\Order;
 
 class Delivery_Date_Picker {
 
     public function __construct() {
-       
         add_action( 'easycommerce-before_cart_summary' , [ $this , 'picker' ] );
-       
-
+        add_action( 'easycommerce_after_order', [ $this, 'save_delivery_date_time' ], 10, 2 );
     }
 
     public function picker() {
         $picker = new Picker;
         $picker->date_time_picker();
     }
+
+    public function save_delivery_date_time( $order_id , $params ) {
+
+        $order = new Order( $order_id );
+        error_log(print_r($order));
+ 
+        if ( isset( $params['date'] ) && isset( $params['time'] ) ) {
+
+            $delivery_date = sanitize_text_field( $params['date'] );
+            $delivery_time = sanitize_text_field( $params['time'] );   
+        }
+
+        $order->add_meta( 'delivery_date', $delivery_date );
+        $order->add_meta( 'delivery_time', $delivery_time );
+
+    }
+
 }
 
 new Delivery_Date_Picker();
--- inc/picker.php
+++ inc/picker.php
@@ -4,7 +4,6 @@ class Picker {
 
     public function date_time_picker() {
         ?>
-       
             <form action="" method="post">
                 <h2>Delivery Date Picker</h2>
                 <label for="">Date : </label>
@@ -12,36 +11,9 @@ class Picker {
                 <label for="">Time : </label>
                 <input type="time" name="time" id="">
                 <br>
-                <!-- <input type="submit" value="Submit" name="submit_delivery"> -->
             </form>
-        
-    
         <?php
-    
-        // if (isset($_POST['submit_delivery'])) {
-    
-        //     global $wpdb;
-        //     $table_name = $wpdb->prefix . 'date_time';
-    
-        //     $data = array(
-        //         'date' => sanitize_text_field($_POST['date']),
-        //         'time' => sanitize_text_field($_POST['time'])
-        //     );
-        //     $format = array('%s', '%s');
-    
-        //     $wpdb->insert($table_name, $data, $format);
-    
-        // } 
     }
 }
-
-
-
-
-
-
-
-
-
 
 ?>
